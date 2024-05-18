5203807a56582df105b82d59ad402f09c67cb459
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-18 21:07:40 +0200

    dying 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,8 +5,11 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("YOU DIED!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 # This function will execute when the timer is timed out.
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
