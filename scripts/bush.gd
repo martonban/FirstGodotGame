a3d52820646d653ca17a686cd753536d9ef684b4
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-06-04 20:37:38 +0200

    Better Bush Shader - WIP

--- scripts/bush.gd
+++ scripts/bush.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @onready var sprite = $Sprite2D
+@onready var timer = $Timer
+
 
 
 func _on_body_entered(body):
-	sprite.material.set_shader_parameter("speed", 5)
+	sprite.material.set_shader_parameter("speed_uniform", 5.0)
+	$Timer.start();
+
+func _on_timer_timeout():
+	sprite.material.set_shader_parameter("speed_uniform", 1.0)
+	
